61fd4aa0cbdb352c71f8fe58d6b5de40631a71d0
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-22 20:15:13 +0545

    Update honey.py

--- honey.py
+++ honey.py
@@ -1,1 +1,15 @@
+'''
+STUDENT NAME: DENISH MAHARJAN
+PROJECT ASSIGNED DATE: 23/06/2024
+PROJECT SUBMISSION DATE: 23/07/2024
+PROJECT TITLE: Honeypot
 
+'''
+
+
+#!/usr/bin/env python
+import logging
+import os
+import socket
+import argparse
+import threading
